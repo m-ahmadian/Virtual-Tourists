59dd75030f6d9aa7520feb1097d9149fc7cce894
Mehrdad <mehrdad.ahmadian@gmail.com>
2021-05-03 21:18:54 +0430

    first commit

---
+++ Virtual Tourist/TravelLocationsMapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelLocationsMapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Mehrdad on 2021-05-03.
+//  Copyright © 2021 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class TravelLocationsMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

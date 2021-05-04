685ff82ec2564554e2d71f6080f742d302c224b9
Mehrdad <mehrdad.ahmadian@gmail.com>
2021-05-04 21:36:27 +0430

    feat: Add a mapView & gestureRecognizer to drop pin

--- Virtual Tourist/TravelLocationsMapViewController.swift
+++ Virtual Tourist/TravelLocationsMapViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class TravelLocationsMapViewController: UIViewController {
-
+    
+    // MARK: - Outlet
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: - Properties
+    var annotations = [MKPointAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    // MARK: - Actions
+    
+    @IBAction func addPin(_ sender: UILongPressGestureRecognizer) {
+        
+        let location = sender.location(in: self.mapView)
+        let locationCoordinate = self.mapView.convert(location, toCoordinateFrom: self.mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationCoordinate
+        annotation.title = "New Title"
+        annotation.subtitle = "New Detail"
+        
+        annotations.append(annotation)
+        self.mapView.addAnnotations(annotations)
+    }
+    
 }
 
